250ebf00a342842b9f2adaf7dc0bc6f0414eee75
remisB <bauzys@gmail.com>
2026-06-02 17:13:26 +0300

    updated example to produce multiple lines in the stacktrace

--- examples/main.go
+++ examples/main.go
@@ -8,16 +8,31 @@ import (
 )
 
 func main() {
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	})
-
+	//handler := newTextHandler()
+	handler := newJSONHandler()
 	logger := slog.New(handler)
 
 	// Usage examples
-	err1 := trace.New("database connection failed")
-	logger.Error("service initialization failed", slog.Any("error", err1))
+	err := errInFunction()
+	logger.Error("service initialization failed", slog.Any("error", err))
 
-	err2 := trace.Wrap(err1, "failed to start server")
+	err2 := trace.Wrap(err, "failed to start server")
 	logger.Error("startup failed", slog.Any("error", err2))
+}
+
+func newTextHandler() slog.Handler {
+	return slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+}
+
+func newJSONHandler() slog.Handler {
+	return slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+}
+
+func errInFunction() error {
+	err := trace.New("database connection failed")
+	return err
 }
